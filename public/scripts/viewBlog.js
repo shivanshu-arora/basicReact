51596f15599ddc622aa2c697eac2e7e2295e6dd2
shivangi <shivangi@UbuntuImage.myguest.virtualbox.org>
2023-08-12 17:01:51 +0530

    End-to-End working code

--- public/scripts/viewBlog.js
+++ public/scripts/viewBlog.js
@@ -3,20 +3,21 @@ const url="http://localhost:3000/view";
 fetch(url)           
   .then(response => response.json())
   .then(data => {
+    //console.log(data);
     var ul = document.createElement('ul');
     data.posts.forEach(post => {
       var li = document.createElement('li');
       li.className = 'viewBlogBox';
       li.innerHTML = `
         <div class="blogBox">
-          <h1>${post.Title}</h1>
-          <h2>${post.Category}</h2>
+          <h1>${post.title}</h1>
+          <h2>${post.category}</h2>
         </div>
         <div class="blogContent">
-          ${post.Content}
+          ${post.text}
         </div>
         <div class="votes">
-            <span>${post.Votes} likes &nbsp <a><i class="fa vote">&#xf087;</i></a> &nbsp <a><i class="fa vote">&#xf088;</i></a></span>
+            <span>1 likes &nbsp <a><i class="fa vote">&#xf087;</i></a> &nbsp <a><i class="fa vote">&#xf088;</i></a></span>
         </div>
       `
     ul.appendChild(li);
